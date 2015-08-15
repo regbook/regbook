cf68fd393978595c63fd12114e127b17bace849b
youhj <youhyunjo@ssu.ac.kr>
2015-08-15 23:28:26 +0900

    R: modsel.R -- functions for regsubsets object

--- R/modsel.R
+++ R/modsel.R
@@ -32,3 +32,66 @@ press.lm <- function(model) {
     pr.r.sq <- 1 - prss / sst
     list(PRESS=prss, pred.r.squared=pr.r.sq)
 }
+
+
+
+##' Fancy Summary
+##' 
+##' @export
+summaryf <- function(object, ...) UseMethod("summaryf")
+
+##' Alternative summary for regsubsets object
+##'
+##' @param object a \code{\link[leaps]{regsubsets}} object
+##' @param ...
+##' @export
+summaryf.regsubsets <- function(object, ...) {
+    smry <- summary(object, ...)
+    class(smry) <- "summaryf.regsubsets"
+    smry
+}
+
+##' @rdname summaryf.regsubsets
+##' @export
+print.summaryf.regsubsets <- function(object, ...)  {
+    with(object,
+         print.data.frame(data.frame(outmat, rss, rsq, adjr2, cp, bic)))
+}
+
+##' Compute Predicted Residual Sum of Squares
+##' 
+##' @param object a \code{\link[leaps]{regsubsets}} object
+##' @param ... 
+##' @export
+press.regsubsets <- function(object, ...) {
+    vars <- all.vars(rgs$call)
+    respname <- vars[1]
+
+    dfname <- vars[length(vars)]
+    x <- eval(as.name(dfname))
+
+    inds <- summary(object)$which[,-1]
+    cnames <- colnames(inds)
+
+    n <- nrow(inds)
+    PRESS <- numeric(n)
+    pred.r.squared <- numeric(n)
+    for (i in 1:n) {
+        ind <- inds[i, ]
+        fit <- lm(as.formula(paste(respname, paste(cnames[ind], collapse="+"), sep="~")), x)
+        pr <- press(fit)
+        PRESS[i] <- pr$PRESS
+        pred.r.squared[i] <- pr$pred.r.squared
+    }
+    
+    ans <- list(which=inds, PRESS=PRESS, pred.r.squared=pred.r.squared, outmat=summary(object)$outmat, obj=object)
+    class(ans) <- "press.regsubsets"
+    ans
+}
+
+##' @rdname press.regsubsets
+##' @param ... 
+##' @export
+print.press.regsubsets <- function(object, ...) {
+    with(object, print.data.frame(data.frame(outmat, PRESS, pred.r.squared)))
+}
